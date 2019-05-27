4e8206dbe9d281c61f9dda2e69367e6690bb89bc
Andrew Owino <owino.andrew@nobilisltd.co.ke>
2019-05-27 18:39:51 +0300

    Introduce the Num class

--- number_to_text.rb
+++ number_to_text.rb
@@ -15,70 +15,70 @@
 # Input: 45781:
 # Ouput: Forty Five Thousand Seven Hundred Eighty One" 
 
-def number_to_text(number)
-	return "zero" if number == 0				
-	# return "Works for upto 900000" if number > 900000				
+class Num
+	def number_to_text(number)
+		return "zero" if number == 0				
+		num_length = number.to_s.length
 
-	num_length = number.to_s.length
+		number_values = {1 => "one", 2 => "two", 3 => "three", 4 => "four", 5 => "five", 
+						6 => "six", 7 => "seven", 8 => "eight", 9 => "nine",
+						10 => "ten" , 11 => "eleven", 12 => "twelve", 13 => "thirteen",
+						14 => "fourteen", 15 => "fifteen", 16 => "sixteen", 17 => "seventeen",
+						18 => "eighteen", 19 => "nineteen"}
 
-	number_values = {1 => "one", 2 => "two", 3 => "three", 4 => "four", 5 => "five", 
-					6 => "six", 7 => "seven", 8 => "eight", 9 => "nine",
-					10 => "ten" , 11 => "eleven", 12 => "twelve", 13 => "thirteen",
-					14 => "fourteen", 15 => "fifteen", 16 => "sixteen", 17 => "seventeen",
-					18 => "eighteen", 19 => "nineteen"}
+		tens = { 20 => "twenty", 30 => "thirty", 40 => "forty", 
+						50 => "fifty", 60 => "sixty" ,70 => "seventy",80 => "eighty",90 => "ninety"}
 
-	tens = { 20 => "twenty", 30 => "thirty", 40 => "forty", 
-					50 => "fifty", 60 => "sixty" ,70 => "seventy",80 => "eighty",90 => "ninety"}
+		multiples = {100 => "hundred", 1000 => "thousand"}
+		
+		return number_values[number] if num_length == 1 
 
-	multiples = {100 => "hundred", 1000 => "thousand"}
-	
-	return number_values[number] if num_length == 1 
+		rest_of_digits = number.to_s[1..num_length - 1].to_i
 
-	rest_of_digits = number.to_s[1..num_length - 1].to_i
-
-	first_number = number.to_s[0].to_i
-	tenths_number = number.to_s[num_length - 2].to_i
-	last_number = number.to_s[num_length - 1].to_i
+		first_number = number.to_s[0].to_i
+		tenths_number = number.to_s[num_length - 2].to_i
+		last_number = number.to_s[num_length - 1].to_i
 
 
-	case num_length
-	when 2
-		return number_values[number] if number < 20
-		if last_number == 0
-			return tens[first_number * 10] + " "
-		else	
-			return tens[first_number * 10] + " " + number_values[last_number.to_i]
-		end		
-	when 3
-		if tenths_number > 1
-			return number_values[first_number] + " hundred and " + tens[tenths_number * 10] + " " + number_values[last_number.to_i]
-		else
+		case num_length
+		when 2
+			return number_values[number] if number < 20
 			if last_number == 0
-				return number_values[first_number] + " hundred"
+				return tens[first_number * 10] + " "
+			else	
+				return tens[first_number * 10] + " " + number_values[last_number.to_i]
+			end		
+		when 3
+			if tenths_number > 1
+				return number_values[first_number] + " hundred and " + tens[tenths_number * 10] + " " + number_values[last_number.to_i]
+			else
+				if last_number == 0
+					return number_values[first_number] + " hundred"
+				else
+					return number_values[first_number] + " hundred and " + number_to_text(rest_of_digits).to_s
+				end
+			end		
+		when 4
+			if last_number == 0
+				return number_values[first_number] + " thousand"
+			else
+				return number_values[first_number] + " thousand and " + number_to_text(rest_of_digits).to_s
+			end		
+		when 5
+			return tens[first_number * 10] + " thousand"
+		when 6
+			if last_number == 0
+				return number_values[first_number] + " hundred thousand"
 			else
 				return number_values[first_number] + " hundred and " + number_to_text(rest_of_digits).to_s
-			end
-		end		
-	when 4
-		if last_number == 0
-			return number_values[first_number] + " thousand"
+			end		
 		else
-			return number_values[first_number] + " thousand and " + number_to_text(rest_of_digits).to_s
-		end		
-	when 5
-		return tens[first_number * 10] + " thousand"
-	when 6
-		if last_number == 0
-			return number_values[first_number] + " hundred thousand"
-		else
-			return number_values[first_number] + " hundred and " + number_to_text(rest_of_digits).to_s
-		end		
-	else
-		return "Not supported."
+			return "Not supported."
+		end
 	end
 end
 
-puts number_to_text(1000)
+puts Num.new.number_to_text(1000)
 # => Twenty three.
 
 # 875
--- spec.rb
+++ spec.rb
@@ -1,18 +1,19 @@
 require "minitest/autorun"
 require_relative "number_to_text"
 
-describe "Number to text" do
+describe "Num.new" do
+	num = Num.new
 	it "should return a number in words" do
-		number_to_text(0).must_equal "zero".downcase
-		number_to_text(23).must_equal "Twenty three".downcase
-		number_to_text(13).must_equal "thirteen".downcase
-		number_to_text(213).must_equal "two hundred and thirteen".downcase
-		number_to_text(9003).must_equal "nine thousand and three".downcase
-		number_to_text(9233).must_equal "nine thousand and two hundred and thirty three".downcase
-		number_to_text(9000).must_equal "nine thousand".downcase
-		number_to_text(3).must_equal "three".downcase
-		number_to_text(50000).must_equal "fifty thousand".downcase
-		number_to_text(500000).must_equal "five hundred thousand".downcase
-		number_to_text(2313).must_equal "two thousand and three hundred and thirteen".downcase
+		num.number_to_text(0).must_equal "zero".downcase
+		num.number_to_text(23).must_equal "Twenty three".downcase
+		num.number_to_text(13).must_equal "thirteen".downcase
+		num.number_to_text(213).must_equal "two hundred and thirteen".downcase
+		num.number_to_text(9003).must_equal "nine thousand and three".downcase
+		num.number_to_text(9233).must_equal "nine thousand and two hundred and thirty three".downcase
+		num.number_to_text(9000).must_equal "nine thousand".downcase
+		num.number_to_text(3).must_equal "three".downcase
+		num.number_to_text(50000).must_equal "fifty thousand".downcase
+		num.number_to_text(500000).must_equal "five hundred thousand".downcase
+		num.number_to_text(2313).must_equal "two thousand and three hundred and thirteen".downcase
 	end
 end
